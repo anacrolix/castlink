13f6f10e17c054eea4cab6e239814ffeaa807c1a
Matt Joiner <anacrolix@gmail.com>
2017-01-17 13:21:30 +1100

    Restructure in progress

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-var session;
 var parsedFragment = parseFragment();
-var loading;
+var receiverApplicationId = '911A4C88';
+var cf;
+var rp;
+var rpc;
 
 setInterval(function() {
     updateProgress();
 }, 1000);
 
 window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
+    show('#api-not-available', false);
     if (loaded) {
         apiAvailable();
     } else {
@@ -17,47 +20,89 @@ window['__onGCastApiAvailable'] = function(loaded, errorInfo) {
     }
 }
 
+function context() {
+    return cast.framework.CastContext.getInstance();
+}
+
+function session() {
+    return context().getCurrentSession();
+}
+
+function media() {
+    var s = session();
+    return s && s.getMediaSession();
+}
+
 function apiAvailable() {
-    $('#receivers').show();
-    var sessionRequest = new chrome.cast.SessionRequest('911A4C88');
-    var apiConfig = new chrome.cast.ApiConfig(
-        sessionRequest,
-        gotSession,
-        gotReceiverAvailability,
-        chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
-        chrome.cast.DefaultActionPolicy.CAST_THIS_TAB);
-    chrome.cast.initialize(apiConfig, apiInitialized, onError);
+    cf = cast.framework;
+    context().setOptions({
+        receiverApplicationId: receiverApplicationId,
+        autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
+        resumeSavedSession: true
+    });
+    context().addEventListener(cast.framework.CastContextEventType.CAST_STATE_CHANGED, castStateChanged);
+    context().addEventListener(cast.framework.CastContextEventType.SESSION_STATE_CHANGED, sessionStateChanged);
+    initRemotePlayer();
+    updateUI();
 };
 
-function gotReceiverAvailability(availability) {
-    if (availability == chrome.cast.ReceiverAvailability.AVAILABLE) {
-        $('#available').show();
-        $('#unavailable').hide();
-        $('#session').show();
-    } else {
-        $('#available').hide();
-        $('#unavailable').show();
-        $('#session').hide();
-    }
+function initRemotePlayer() {
+    rp = new cf.RemotePlayer();
+    rpc = new cf.RemotePlayerController(rp);
+    rpc.addEventListener(cf.RemotePlayerEventType.ANY_CHANGE, remotePlayerChanged);
+}
+
+function remotePlayerChanged(event) {
+    console.log('remote player changed', event);
+    media() && media().addUpdateListener(mediaUpdated);
+    updateUI();
+}
+
+function castStateChanged(castState) {
+    console.log('cast state changed: ', castState);
+    updateUI();
+}
+
+function sessionStateChanged(s) {
+    console.log('session state changed: ', s);
+    updateUI();
+    // Might be starting or ended, and so there's no session.
+    if (!s.session) return;
+    s.session.addEventListener(cast.framework.SessionEventType.MEDIA_SESSION, mediaSessionChanged);
+}
+
+function mediaSessionChanged(m) {
+    console.log('media session changed', m);
+    m.mediaSession.addUpdateListener(mediaUpdated)
+}
+
+function mediaUpdated(alive) {
+    console.log('media status changed', alive);
+    updateUI();
+}
+
+function receiversAvailable(available) {
+    show('#available', available);
+    show('#unavailable', !available);
+    show('#session', available);
 }
 
 function loadMedia(spec) {
     var url = spec.url;
     var title = spec.title;
-    var subtitles = spec.subtitles;
     var subtitle = spec.subtitle;
     var poster = spec.poster;
-    var tracks = [];
-    if (subtitles) {
-        var enSubs = new chrome.cast.media.Track(1, chrome.cast.media.TrackType.TEXT);
-        enSubs.trackContentId = subtitles;
-        enSubs.trackContentType = 'text/vtt';
-        enSubs.subtype = chrome.cast.media.TextTrackType.SUBTITLES;
-        enSubs.language = 'en-US';
-        tracks.push(enSubs);
-    }
-    var mediaInfo = new chrome.cast.media.MediaInfo(url);
-    mediaInfo.contentType = 'video/mp4';
+    // var tracks = [];
+    // if (subtitles) {
+    //     var enSubs = new chrome.cast.media.Track(1, chrome.cast.media.TrackType.TEXT);
+    //     enSubs.trackContentId = subtitles;
+    //     enSubs.trackContentType = 'text/vtt';
+    //     enSubs.subtype = chrome.cast.media.TextTrackType.SUBTITLES;
+    //     enSubs.language = 'en-US';
+    //     tracks.push(enSubs);
+    // }
+    var mediaInfo = new chrome.cast.media.MediaInfo(url, 'video/mp4');
+    // mediaInfo.contentType = 'video/mp4';
     var metadata = new chrome.cast.media.GenericMediaMetadata();
     if (poster) {
         metadata.images = [new chrome.cast.Image(poster)];
@@ -68,15 +113,20 @@ function loadMedia(spec) {
     }
     metadata.subtitle = (subtitle || url).substr(0, 100);
     mediaInfo.metadata = metadata;
-    mediaInfo.tracks = tracks;
-    mediaInfo.textTrackStyle = new chrome.cast.media.TextTrackStyle();
+    // mediaInfo.tracks = tracks;
+    // mediaInfo.textTrackStyle = new chrome.cast.media.TextTrackStyle();
     var request = new chrome.cast.media.LoadRequest(mediaInfo);
-    if (subtitles) {
-        request.activeTrackIds = [1];
-    }
-    session.loadMedia(request, mediaLoaded, mediaLoadError);
-    loading = spec;
+    // if (subtitles) {
+    //     request.activeTrackIds = [1];
+    // }
+    request.autoplay = true;
+    console.log(session(), request);
+    session().loadMedia(request).then(mediaLoaded, mediaLoadError);
     updateUI();
+}
+
+function mediaLoaded() {
+    console.log('media loaded');
 }
 
 function mediaLoadError(e) {
@@ -86,84 +136,17 @@ function mediaLoadError(e) {
 
 function displayControl(sel, display) {
     var j = $(sel);
-    if (!j.length) throw sel;
+    if (!j.length) console.log('control not found', sel);
     if (display) j.show();
     else j.hide();
 }
 
 var show = displayControl;
 
-function showLeave() {
-    $('.leave').show();
-}
-
-function hideLeave() {
-    $('.leave').hide();
-}
-
-function displayJoin(display) {
-    if (display) showJoin();
-    else hideJoin();
-}
-
-function showJoin() {
-    $('.join').show();
-}
-
-function hideJoin() {
-    $('.join').hide();
-}
-
-function showPlay() {
-    $('.play').show();
-}
-
-function hidePlay() {
-    $('.play').hide();
-}
-
-function showPause() {
-    $('.pause').show();
-}
-
-function hidePause() {
-    $('.pause').hide();
-}
-
-function showStop() {
-    $('.stop').show();
-}
-
-function hideStop() {
-    $('.stop').hide();
-}
-
-function showProgress() {
-    $('#progress').show();
-}
-
-function hideProgress() {
-    $('#progress').hide();
-}
-
-function gotSession(s) {
+function gotSession() {
+    var s = session();
     console.log('got session', s);
-    session = s;
-    session.addUpdateListener(sessionUpdated);
-    session.media.forEach(function(m) {
-        m.addUpdateListener(mediaUpdated);
-    });
-    if (!activeMedia()) {
-        loadProposedMedia();
-    }
-    updateUI();
-}
-
-function sessionUpdated(isAlive) {
-    console.log('session updated', isAlive, session);
-    session.media.forEach(function(m) {
-        m.addUpdateListener(mediaUpdated);
-    });
+    // s.addEventListener(sessionUpdated);
     updateUI();
 }
 
@@ -180,26 +163,18 @@ function updateProgress() {
     if (!media) {
         return;
     }
-    $('#progress-bar').attr({
-        value: media.getEstimatedTime(),
-        max: media.media.duration
-    });
+    var width = 100*media.getEstimatedTime()/media.media.duration + '%';
+    $('#progress-bar').css('width', width);
+    // $('#progress-bar').attr({
+    //     value: media.getEstimatedTime(),
+    //     max: media.media.duration
+    // });
     $('.current-time').text(toHHMMSS(media.getEstimatedTime()));
     $('.duration').text(toHHMMSS(media.media.duration));
 }
 
-function mediaUpdated(alive) {
-    console.log('media updated', alive);
-    updateUI();
-}
-
 function activeMedia() {
-    if (!session) return;
-    if (session.status != chrome.cast.SessionStatus.CONNECTED) return;
-    var media = session.media[0];
-    if (!media) return;
-    if (media.playerState == chrome.cast.media.PlayerState.IDLE) return;
-    return media;
+    return media();
 }
 
 function proposedContentURL() {
@@ -219,81 +194,106 @@ function isProposedMedia(m) {
 }
 
 function updateUI() {
-    var sessionConnected = session && session.status == chrome.cast.SessionStatus.CONNECTED;
-    show('#request-session', !sessionConnected);
-    show('#stop-session', sessionConnected);
-    show('#leave-session', sessionConnected);
-    show('#loaded', sessionConnected);
-    displayControl('#leave-session', sessionConnected);
-    var media = activeMedia();
-    show('#loaded', media);
-    if (media) {
-        $('#current-url').text(media && media.media.contentId || 'none');
-        displayControl('#play-button', function() {
-            return media.playerState == chrome.cast.media.PlayerState.PAUSED;
-        }());
-        displayControl('#pause-button', function() {
-            return media.playerState != chrome.cast.media.PlayerState.PAUSED;
-        }());
-        displayControl('#stop-button', function() {
-            return true;
-        }());
-    }
-    show('#load-button', sessionConnected && !isProposedMedia(media) && !loading);
-    show('#loading-button', loading);
-    show('#reload-button', sessionConnected && isProposedMedia(media));
+    console.log('updating ui');
+    show('#no-devices-available', context().getCastState() == cf.CastState.NO_DEVICES_AVAILABLE);
+    show('#connected', context().getCastState() == cf.CastState.CONNECTED);
+    $('#connected-receiver-name').text(session() && session().getCastDevice().friendlyName);
+    show('#request-session-button', _in(context().getSessionState(), cf.SessionState.NO_SESSION, cf.SessionState.SESSION_ENDED));
+    show('#leave-session-button', _in(context().getSessionState(), cf.SessionState.SESSION_STARTED, cf.SessionState.SESSION_RESUMED));
+    show('#stop-session-button', _in(context().getSessionState(), cf.SessionState.SESSION_STARTED, cf.SessionState.SESSION_RESUMED));
+    var m = media();
+    var ss = session() && session().getSessionState();
+    var ps = m && m.playerState;
+    console.log(ps);
+    show('#pause-button', ps && _in(ps, 'PLAYING', 'BUFFERING'));
+    show('#play-button', ps && !_in(ps, 'PLAYING', 'IDLE'));
+    show('#stop-button', ps && ps != 'IDLE');
+    // show('#loading-button', loading);
+    show('#player', rp.isMediaLoaded);
+    show('#loaded-button', rp.isMediaLoaded);
+    $1('#media-form .load').prop('disabled', !session());
     updateProgress();
 }
 
-function mediaLoaded(m) {
-    console.log('media loaded', m);
-    loading = null;
-    m.addUpdateListener(mediaUpdated);
-    updateUI();
-}
-
-$(document).ready(function() {
-    $('#progress-bar').on('click', function(e) {
+function setClickHandlers() {
+    $1('#progress-bar').on('click', function(e) {
         var sr = new chrome.cast.media.SeekRequest();
         sr.currentTime = e.offsetX/e.target.clientWidth*activeMedia().media.duration;
         activeMedia().seek(sr, null, onError);
     });
-    $('#pause-button').on('click', function() {
+    $1('#pause-button').on('click', function() {
+        // rpc.playOrPause();
         activeMedia().pause(null, null, onError);
     });
-    $('#play-button').on('click', function() {
+    $1('#play-button').on('click', function() {
+        // rpc.playOrPause();
         activeMedia().play(null, null, onError);
     });
-    $('#stop-button').on('click', function() {
-        activeMedia().stop(null, null, onError);
+    $1('#stop-button').on('click', function() {
+        // activeMedia().stop(null, null, onError);
+        // media().stop();
+        rpc.stop();
     });
-    $('#leave-session').on('click', function() {
-        session.leave(null, onError);
+    $1('#leave-session-button').on('click', function() {
+        context().endCurrentSession(false);
     });
-    $('#stop-session').on('click', function() {
-        session.stop(null, onError);
+    $1('#stop-session-button').on('click', function() {
+        context().endCurrentSession(true);
     });
-    $('#request-session').on('click', function() {
-        chrome.cast.requestSession(gotSession, onError);
+    $1('#request-session-button').on('click', function() {
+        context().requestSession().then(gotSession, onError);
     });
-    $('#new-url').val(parsedFragment.getLast('content') || 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4');
-    $('#subtitles').val(parsedFragment.getLast('subtitles'));
-    $('#load-button').on('click', function() {
-        loadProposedMedia();
-    });
-    $('#reload-button').on('click', function() {
-        loadProposedMedia();
-    });
-    $('#proposed textarea').on('click', function() {
+    $1('#new-url').val();
+    $1('#subtitles').val(parsedFragment.getLast('subtitles'));
+    $1('#proposed textarea').on('click', function() {
         $(this).select();
     });
-    updateUI();
+    $1('#media-form .load').on('click', function() {
+        loadMedia(getMediaSpecFromForm());
+    });
+}
+
+$(document).ready(function() {
+    setClickHandlers();
+    $('#media-form').on('show.bs.modal', function(event) {
+        var id = event.relatedTarget.id;
+        console.log(id);
+        switch (id) {
+        case 'proposed-button':
+            setMediaFormFromSpec(proposedMediaSpec());
+            break;
+        case 'loaded-button':
+            setMediaFormFromSpec(loadedMediaSpec());
+            break
+        default:
+            throw(id);
+        }
+    });
 });
+
+function setMediaFormFromSpec(spec) {
+    console.log(spec);
+    $1('#media-form .content.url').val(spec.url);
+    $1('#media-form .subtitles.url').val(spec.subtitles);
+    $1('#media-form .title').val(spec.title);
+    $1('#media-form .poster').val(spec.poster);
+    $1('#media-form .subtitle').val(spec.subtitle);
+}
+
+function getMediaSpecFromForm() {
+    return {
+        url: $('#media-form .content').val(),
+        subtitles: $('#media-form .subtitles').val(),
+        title: $('#media-form .title').val(),
+        poster: $('#media-form .poster').val(),
+        subtitle: $('#media-form .subtitle').val()
+    };
+}
 
 function proposedMediaSpec() {
     return {
-        url: $('#new-url').val(),
-        subtitles: $('#subtitles').val(),
+        url: parsedFragment.getLast('content') || 'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
+        subtitles: parsedFragment.getLast('subtitles'),
         title: parsedFragment.getLast('title'),
         poster: parsedFragment.getLast('poster'),
         subtitle: parsedFragment.getLast('subtitle')};
@@ -341,4 +341,32 @@ function toHHMMSS(i) {
     if (minutes < 10) {minutes = "0"+minutes;}
     if (seconds < 10) {seconds = "0"+seconds;}
     return hours+':'+minutes+':'+seconds;
+}
+
+function _in(a) {
+    for (var i = 1; i < arguments.length; i++) {
+        if (arguments[i] == a) return true;
+    }
+    return false;
+}
+
+function $1(sel) {
+    var j = $(sel);
+    if (!j.length) console.log('selection matched nothing', sel);
+    return j;
+}
+
+var $plus = $1;
+
+function loadedMediaSpec() {
+    var m = media();
+    var mi = m.media;
+    var md = mi.metadata;
+    return {
+        url: mi.contentId,
+        subtitles: mi.tracks && mi.tracks[0].trackContentId,
+        poster: md.images && md.images[0].url,
+        title: md.title,
+        subtitle: md.subtitle
+    };
 }
